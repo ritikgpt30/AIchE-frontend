e6ce398dd6fdf84b50362f3a2fba22fc442f3b36
ritik <ritikgpt30@gmail.com>
2020-02-15 22:32:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import 'antd/dist/antd.css'
 
 import HomeIndex from './components/Landingpage/LandingIndex';
+import NewsIndex from './components/Newspage/NewsIndex';
 
 
 
@@ -18,6 +19,7 @@ class App extends Component {
       <BrowserRouter>
         <React.Fragment>
           <Route exact path="/" component={HomeIndex} />
+          <Route exact path="/news" component={NewsIndex} />
         </React.Fragment>
       </BrowserRouter>
     );
